6d92d945fd0cc38aead3315acc886c8ac69c764a
tristanpendergrass <tristanpendergrass@gmail.com>
2020-03-26 14:30:56 -0700

    Add ConstructAxe and BuildHut modules

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (Html, button, div, h1, h2, hr, text)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, disabled)
 import Html.Events exposing (onClick)
 
 
@@ -13,35 +13,94 @@ type MainQuestStage
 
 
 type Module
-    = MainQuest MainQuestStage
+    = MainQuest
     | Stockpile
     | GatherWood
+    | ConstructAxe
+    | BuildHut
 
 
 type alias Model =
     { modules : List Module
+    , mainQuestStage : MainQuestStage
     , wood : Int
+    , hasAxe : Bool
     }
 
 
 initialModel : Model
 initialModel =
-    { modules = [ GatherWood, Stockpile, MainQuest One ]
+    { modules =
+        List.reverse
+            [ MainQuest
+            , Stockpile
+            , GatherWood
+            , ConstructAxe
+            ]
+    , mainQuestStage = One
     , wood = 0
+    , hasAxe = False
     }
 
 
 type Msg
     = HandleGatherWood
+    | HandleConstructAxe
+    | HandleBuildHut
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         HandleGatherWood ->
+            let
+                amountGathered : Int
+                amountGathered =
+                    if model.hasAxe then
+                        10
+
+                    else
+                        1
+            in
             { model
-                | wood = model.wood + 1
+                | wood = model.wood + amountGathered
             }
+
+        HandleConstructAxe ->
+            let
+                newModules : List Module
+                newModules =
+                    model.modules
+                        |> List.filter (\mod -> mod /= ConstructAxe)
+                        |> (::) BuildHut
+            in
+            if model.wood >= 10 then
+                { model
+                    | wood = model.wood - 10
+                    , hasAxe = True
+                    , modules = newModules
+                    , mainQuestStage = Two
+                }
+
+            else
+                model
+
+        HandleBuildHut ->
+            let
+                newModules : List Module
+                newModules =
+                    model.modules
+                        |> List.filter (\mod -> mod /= BuildHut)
+            in
+            if model.wood >= 10 then
+                { model
+                    | wood = model.wood - 100
+                    , modules = newModules
+                    , mainQuestStage = Completed
+                }
+
+            else
+                model
 
 
 renderMainQuest : MainQuestStage -> Html Msg
@@ -54,7 +113,7 @@ renderMainQuest stage =
                     "Stage 1 (craft an axe to advance to Stage 2)"
 
                 Two ->
-                    "Stage 2 (build a hut to advance to Complete the Game"
+                    "Stage 2 (build a hut to Complete the Game"
 
                 Completed ->
                     "You have completed the game"
@@ -73,11 +132,35 @@ renderStockpile wood =
         ]
 
 
-renderGatherWood : Html Msg
-renderGatherWood =
+renderGatherWood : Bool -> Html Msg
+renderGatherWood hasAxe =
     div [ class "gather-wood" ]
         [ h2 [ class "module-title" ] [ text "Gather Wood" ]
-        , button [ onClick HandleGatherWood ] [ text "Forage for sticks" ]
+        , button [ onClick HandleGatherWood ]
+            [ text
+                (if hasAxe then
+                    "Chop Wood"
+
+                 else
+                    "Forage for sticks"
+                )
+            ]
+        ]
+
+
+renderConstructAxe : Int -> Html Msg
+renderConstructAxe wood =
+    div [ class "construct-axe" ]
+        [ h2 [ class "module-title" ] [ text "Construct Axe" ]
+        , button [ onClick HandleConstructAxe, disabled (wood < 10) ] [ text "Construct for 10 Wood" ]
+        ]
+
+
+renderBuildHut : Int -> Html Msg
+renderBuildHut wood =
+    div [ class "build-hut" ]
+        [ h2 [ class "module-title" ] [ text "Build Hut" ]
+        , button [ onClick HandleBuildHut, disabled (wood < 100) ] [ text "Build for 100 Wood" ]
         ]
 
 
@@ -87,14 +170,20 @@ renderModule model mod =
         inner : Html Msg
         inner =
             case mod of
-                MainQuest stage ->
-                    renderMainQuest stage
+                MainQuest ->
+                    renderMainQuest model.mainQuestStage
 
                 Stockpile ->
                     renderStockpile model.wood
 
                 GatherWood ->
-                    renderGatherWood
+                    renderGatherWood model.hasAxe
+
+                ConstructAxe ->
+                    renderConstructAxe model.wood
+
+                BuildHut ->
+                    renderBuildHut model.wood
     in
     div [ class "module" ] [ inner ]
 
